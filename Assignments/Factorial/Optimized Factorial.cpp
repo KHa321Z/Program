f7b586e96a1e9f1f70bf8e29d946d4dd8979a540
Hammad Zaeem <70287776+KHa321Z@users.noreply.github.com>
2022-08-14 22:18:55 +0500

    Create Optimized Factorial.cpp

---
+++ Assignments/Factorial/Optimized Factorial.cpp
@@ -0,0 +1,34 @@
+/*
+Factorial of a nonnegative integer n is written as n! (pronounced as “n factorial”) and is
+defined as follows:
+n! = n . (n … 1) . (n … 2) . . .1 or (product of all positive integers from n to 1)
+n! = 1 (for n = 0)
+for example , 5! = 5 . 4 . 3 . 2 . 1
+Write a program that reads a nonnegative integer and prints its factorial.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+long factorial (long nonNegInt) {
+    
+    if (nonNegInt == 1) 
+        return (nonNegInt);
+        
+    else return (nonNegInt * factorial(nonNegInt - 1));
+        
+};
+
+long nonNegativeInt;
+
+int main() {
+    
+    cout << "Enter Non Negative Integer: ";
+    cin >> nonNegativeInt;
+    
+    cout << endl << factorial(nonNegativeInt);
+
+    return 0;
+    
+};
